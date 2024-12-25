0f5802e0ff4a051e05c8010b5268621b8a3f7884
Henry Chan <henry.d.chan@gmail.com>
2024-12-24 22:08:46 -0500

    saving

--- scripts/datacard.py
+++ scripts/datacard.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
-from PyQt5.QtCore import QPropertyAnimation, QRect, QPoint, pyqtProperty, QSize, Qt
+from PyQt5.QtCore import QPropertyAnimation, QRect, QPoint, pyqtProperty, QSize, Qt, QEasingCurve
 from PyQt5.QtGui import QPainter, QColor, QBrush, QPen, QLinearGradient
 import math
 class MyWidget(QWidget):
@@ -13,17 +13,28 @@ class MyWidget(QWidget):
         self.rotatione_animation.setStartValue(0)
         self.rotatione_animation.setEndValue(360)
         self.rotatione_animation.setLoopCount(-1)  # Loop indefinitely
-        # self.rotatione_animation.start()
+        self.rotatione_animation.start()
         
-        
+        start = 0.4
+        self._shine = 0.4
+        self._shine_width = 0.1
+        self.shine_animation = QPropertyAnimation(self, b"shine")
+        # self.shine_animation.setEasingCurve(QEasingCurve.OutInCubic)
+        self.shine_animation.setDuration(5000)  # Animation duration in milliseconds
+        self.shine_animation.setStartValue(start)
+        self.shine_animation.setKeyValueAt(0.5, 0.9)
+        self.shine_animation.setEndValue(start)
+        self.shine_animation.setLoopCount(-1)
+        self.shine_animation.start()
 
     def paintEvent(self, event):
         circle_painter = QPainter(self)
         circle_painter.setRenderHint(QPainter.Antialiasing)
 
-        
-        w = circle_painter.device().width()
-        h = circle_painter.device().height()
+        o_w = circle_painter.device().width()
+        o_h = circle_painter.device().height()
+        w = o_w * 0.8
+        h = o_h * 0.8
         min_d = min(w, h)
         t = min_d * 0.005
         pen = QPen(QColor('#000'), t, cap=Qt.FlatCap)
@@ -33,17 +44,33 @@ class MyWidget(QWidget):
 
         gradient = QLinearGradient(0, 0, w, h)
         gradient.setColorAt(0.00, QColor('#AE8625'))
-        gradient.setColorAt(0.45, QColor('#F7EF8A'))
-        gradient.setColorAt(0.85, QColor('#D2AC47'))
-        gradient.setColorAt(0.95, QColor('#EDC967'))
+        gradient.setColorAt(0.4, QColor('#F7EF8A'))
+        gradient.setColorAt(0.8, QColor('#D2AC47'))
+        gradient.setColorAt(0.9, QColor('#EDC967'))
+        
+        # gradient.setColorAt(0.1, QColor('#DFBD69'))
+        # gradient.setColorAt(self._shine, QColor('#F7EF8A'))
+        # gradient.setColorAt(0.9, QColor('#926F34'))
+        
+        
+        # gradient.setColorAt(0.1, QColor('#F9F295'))
+        # gradient.setColorAt(0.3, QColor('#E0AA3E'))
+        # gradient.setColorAt(0.75, QColor('#FAF398'))
+        # gradient.setColorAt(0.95, QColor('B88A44'))
         circle_painter.setBrush(QBrush(gradient))
+
+        t_center_x, t_center_y = (o_w-w)/2, (o_h-h)/2
 
         def get_size(m):
             return min_d * m - t * 2
+        
+        def local_to_global_midpoint(size):
+            return ((w-size)/2, (h-size)/2)
 
         for m in multipliers:
             size = get_size(m)
-            circle_painter.drawEllipse((w - size) / 2, (h - size) / 2, size, size)
+            x, y = local_to_global_midpoint(size)
+            circle_painter.drawEllipse(x + t_center_x, y + t_center_y, size, size)
 
         line_painter = QPainter(self)
         line_painter.setPen(pen)
@@ -64,13 +91,14 @@ class MyWidget(QWidget):
             size2 = get_size(0.8)
             new_x_2, new_y_2 = (w/2) + (size2 / 2 * math.cos(theta)), (h/2) + (size2 / 2 * math.sin(theta)) 
             
-            line_painter.drawLine(new_x_2, new_y_2, new_x, new_y)
+            line_painter.drawLine(new_x_2 + t_center_x, new_y_2 + t_center_y, new_x + t_center_x, new_y + t_center_y)
         
         arc_painter = QPainter(self)
         arc_painter.setPen(pen)
         arc_painter.setRenderHint(QPainter.Antialiasing)
         size = get_size(0.8)
-        arc_painter.drawArc((w-size)/2, (h-size)/2, size, size, (-30 - self._angle) * 16 - t * 1, (240) * 16 + (t * 2))
+        x, y = local_to_global_midpoint(size)
+        arc_painter.drawArc(x + t_center_x, y + t_center_y, size, size, (-30 - self._angle) * 16 - t * 1, (240) * 16 + (t * 2))
 
 
     @pyqtProperty(int)
@@ -80,6 +108,15 @@ class MyWidget(QWidget):
     @angle.setter
     def angle(self, value):
         self._angle = value
+        self.update()
+        
+    @pyqtProperty(float)
+    def shine(self):
+        return self._shine
+    
+    @shine.setter
+    def shine(self, value):
+        self._shine = value
         self.update()
         
             
