44f4967e8aeedca8c47501abda426cfdbe2373fc
Henry Chan <henry.d.chan@gmail.com>
2025-01-02 23:46:36 -0500

    saving

---
+++ scripts/color.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtCore import Qt
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.black, 2))
+
+        # Draw the original line
+        painter.drawLine(10, 10, 100, 100)
+
+        # Translate the coordinate system
+        painter.translate(500, 500)
+
+        # Draw the translated line (same coordinates as the first line)
+        painter.drawLine(10, 10, 100, 100)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = MyWidget()
+    widget.show()
+    sys.exit(app.exec_())
